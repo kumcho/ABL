0b7c8a40a523f8363becb4444fff001489f2a8dc
Erik <erig.smith.350@gmail.com>
2021-02-05 16:00:23 +0200

    first commit

---
+++ ABL/src/abl/model/Match.java
@@ -0,0 +1,15 @@
+package abl.model;
+
+public class Match {
+	
+	int ID;
+	
+	int home;
+	
+	int guest;
+	
+	int Hpoints;
+	
+	int Gpoints;
+
+}
---
+++ ABL/src/abl/model/Team.java
@@ -0,0 +1,22 @@
+package abl.model;
+
+import java.sql.*;
+
+public class Team {
+
+	int ID;
+	
+	String Name;
+	
+	Date Founded;
+	
+	int NoP;
+	
+	String Coach;
+	
+	int Wins;
+	
+	int Loses;
+	
+	
+}
